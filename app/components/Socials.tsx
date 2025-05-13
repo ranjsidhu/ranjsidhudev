3d9e792fe9c2b389fe9d314817cb961aae5f4f11
Ranjeet Sidhu <ranjsidhudev@gmail.com>
2025-05-13 05:24:56 +0000

    refactor: socials component

---
+++ app/components/Socials.tsx
@@ -0,0 +1,44 @@
+import type { SocialProps } from "@/types";
+import { FaInstagram, FaLinkedin, FaEnvelope, FaGithub } from "react-icons/fa";
+import Link from "next/link";
+
+export default function Socials({ size }: SocialProps) {
+  return (
+    <>
+      <Link
+        href="https://www.instagram.com/ranjsidhu.dev/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Instagram"
+        className="hover:opacity-70 transition"
+      >
+        <FaInstagram size={size} color="#000" />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/ranjsidhu/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="LinkedIn"
+        className="hover:opacity-70 transition"
+      >
+        <FaLinkedin size={size} color="#000" />
+      </Link>
+      <Link
+        href="mailto:ranj@ranjsidhu.dev"
+        aria-label="Email"
+        className="hover:opacity-70 transition"
+      >
+        <FaEnvelope size={size} color="#000" />
+      </Link>
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/ranjsidhu"
+        aria-label="GitHub"
+        className="hover:opacity-70 transition"
+      >
+        <FaGithub size={size} color="#000" />
+      </Link>
+    </>
+  );
+}
